58cca5dd43ba7f76c52eb67358f18031faf49e80
Paul Meinhardt <paul@bitcrowd.net>
2020-04-30 16:01:44 +0200

    Connect to message queue via AMQP

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-process.stdout.write('hello world!');
+const amqp = require('amqplib');
+
+const { AMQP_URI, AMQP_QUEUE } = process.env;
+
+function handler(message) {
+  const payload = JSON.parse(message.content.toString());
+  console.log(payload);
+}
+
+(async function main() {
+  // Connect to the queue
+  const connection = await amqp.connect(AMQP_URI);
+  const channel = await connection.createChannel();
+
+  // Ensure the queue is created
+  await channel.assertQueue(AMQP_QUEUE, { durable: true });
+
+  // Receive messages from the queue
+  await channel.consume(AMQP_QUEUE, handler, {});
+})();

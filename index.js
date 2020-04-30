fdda322493656b50577afae27194cd3ee4e994da
Paul Meinhardt <paul@bitcrowd.net>
2020-04-30 19:42:23 +0200

    Handle signals and attempt an orderly shutdown
    Signals are usually sent by a user, e.g. pressing Ctrl-C for "SIGINT",
    or Kubernetes, which sends a "SIGTERM" to let a container know it is
    going to be shut down soon.

--- index.js
+++ index.js
@@ -2,19 +2,43 @@ const amqp = require('amqplib');
 
 const { AMQP_URI, AMQP_QUEUE } = process.env;
 
-function handler(message) {
-  const payload = JSON.parse(message.content.toString());
-  console.log(payload);
+function trap(signal, handler, code) {
+  process.on(signal, async () => {
+    try {
+      await handler();
+    } catch (_) {
+      process.exit(code);
+    }
+  });
 }
 
 (async function main() {
-  // Connect to the queue
+  // Connect to the queue.
   const connection = await amqp.connect(AMQP_URI);
   const channel = await connection.createChannel();
 
-  // Ensure the queue is created
+  // Gracefully handle signals and initiate an orderly shutdown.
+  // Exit with a signal-specific non-zero code if cleanup fails.
+  // https://nodejs.org/api/process.html#process_exit_codes
+
+  const shutdown = async () => {
+    await channel.close();
+    await connection.close();
+  };
+
+  trap('SIGINT',  shutdown, 128 + 2); // eslint-disable-line prettier/prettier
+  trap('SIGQUIT', shutdown, 128 + 3);
+  trap('SIGTERM', shutdown, 128 + 15);
+
+  // Ensure the queue is created.
   await channel.assertQueue(AMQP_QUEUE, { durable: true });
 
-  // Receive messages from the queue
-  await channel.consume(AMQP_QUEUE, handler, {});
+  // Process each individual message.
+  const handle = (message) => {
+    const payload = JSON.parse(message.content.toString());
+    console.log(payload); // TODO: Acknowledge handled messages
+  };
+
+  // Start to consume queued messages.
+  await channel.consume(AMQP_QUEUE, handle, {});
 })();

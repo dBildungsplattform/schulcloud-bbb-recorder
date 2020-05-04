e2ff1f9e60bc788d7e32263d9d3d8bb132a0bfe7
Paul Meinhard <paul@bitcrowd.net>
2020-05-04 18:43:13 +0200

    Add more tests for main module
    - Test AMQP connection and channel setup
    - Test process signal handling
    - Test message handling

--- main/index.test.js
+++ main/index.test.js
@@ -6,6 +6,19 @@ const record = require('./record');
 jest.mock('amqplib');
 jest.mock('./record');
 
+const processExit = process.exit;
+const processOn = process.on;
+
+function setupProcess() {
+  process.exit = jest.fn();
+  process.on = jest.fn();
+}
+
+function restoreProcess() {
+  process.exit = processExit;
+  process.on = processOn;
+}
+
 function mock(keys, makeFn = () => jest.fn().mockResolvedValue()) {
   return keys.reduce((object, key) => {
     // eslint-disable-next-line no-param-reassign
@@ -16,26 +29,15 @@ function mock(keys, makeFn = () => jest.fn().mockResolvedValue()) {
 
 describe('main', () => {
   async function invoke(overrides) {
-    const defaults = {
+    return main({
       AMQP_URI: 'amqp://localhost.test',
       AMQP_QUEUE: 'test-queue',
-    };
-
-    return main({ ...defaults, ...overrides });
+      ...overrides,
+    });
   }
 
   let connection;
   let channel;
-
-  async function handle(message) {
-    await invoke({}); // invoke "main" to perform setup
-    const [[, handler]] = channel.consume.mock.calls;
-    return handler(message);
-  }
-
-  function marshal(payload) {
-    return Buffer.from(JSON.stringify(payload));
-  }
 
   beforeEach(() => {
     channel = mock('ack assertQueue close consume nack prefetch'.split(' '));
@@ -44,77 +46,124 @@ describe('main', () => {
     connection.createChannel.mockResolvedValue(channel);
 
     amqp.connect.mockResolvedValue(connection);
+
+    setupProcess();
   });
 
   afterEach(() => {
     amqp.connect.mockReset();
     record.mockReset();
+    restoreProcess();
   });
 
-  it('connects to the message broker', async () => {
-    const AMQP_URI = 'amqp://unit:test@localhost:5672';
-    await invoke({ AMQP_URI });
-    expect(amqp.connect).toHaveBeenCalledWith(AMQP_URI);
-  });
+  describe('setup', () => {
+    it('connects to the message broker', async () => {
+      const AMQP_URI = 'amqp://unit:test@localhost:5672';
+      await invoke({ AMQP_URI });
+      expect(amqp.connect).toHaveBeenCalledWith(AMQP_URI);
+    });
 
-  it('creates a channel', async () => {
-    await invoke({});
-    expect(connection.createChannel).toHaveBeenCalled();
-  });
+    it('creates a channel', async () => {
+      await invoke({});
+      expect(connection.createChannel).toHaveBeenCalled();
+    });
 
-  it('asserts the queue into existence', async () => {
-    const AMQP_QUEUE = 'test-queue-name:assertQueue';
-    await invoke({ AMQP_QUEUE });
-    expect(channel.assertQueue).toHaveBeenCalledWith(AMQP_QUEUE, {
-      durable: true,
+    it('asserts the queue into existence', async () => {
+      const AMQP_QUEUE = 'test-queue-name:assertQueue';
+      await invoke({ AMQP_QUEUE });
+      expect(channel.assertQueue).toHaveBeenCalledWith(AMQP_QUEUE, {
+        durable: true,
+      });
+    });
+
+    it('sets the prefetch count to 1', async () => {
+      await invoke({});
+      expect(channel.prefetch).toHaveBeenCalledWith(1);
+    });
+
+    it('sets up a handler for queued messages', async () => {
+      const AMQP_QUEUE = 'test-queue-name:consume';
+      await invoke({ AMQP_QUEUE });
+      expect(channel.consume).toHaveBeenCalledWith(
+        AMQP_QUEUE,
+        expect.any(Function),
+        {}
+      );
     });
   });
 
-  it('sets the prefetch count to 1', async () => {
-    await invoke({});
-    expect(channel.prefetch).toHaveBeenCalledWith(1);
+  describe('message handler', () => {
+    async function handle(message) {
+      await invoke({}); // invoke "main" to perform setup
+      const [[, handler]] = channel.consume.mock.calls;
+      return handler(message);
+    }
+
+    function marshal(payload) {
+      return Buffer.from(JSON.stringify(payload));
+    }
+
+    it('creates a video for each message', async () => {
+      const url = 'https://localhost/playback/123456';
+      const duration = 59;
+      await handle({ content: marshal({ url, duration }) });
+      expect(record).toHaveBeenCalledWith(url, duration);
+    });
+
+    it.todo('uploads the created videos');
+
+    it('acknowledges messages on success', async () => {
+      const payload = marshal({ url: 'https://…', duration: 12 });
+      const message = { id: 'test-success', content: marshal(payload) };
+      await handle(message);
+      expect(channel.ack).toHaveBeenCalledWith(message);
+    });
+
+    it('rejects messages on failure', async () => {
+      record.mockRejectedValue(new Error('Test Error'));
+      const message = { id: 'test-failure' };
+      await handle(message);
+      expect(channel.nack).toHaveBeenCalledWith(message);
+    });
   });
 
-  it('sets up a handler for queued messages', async () => {
-    const AMQP_QUEUE = 'test-queue-name:consume';
-    await invoke({ AMQP_QUEUE });
-    expect(channel.consume).toHaveBeenCalledWith(
-      AMQP_QUEUE,
-      expect.any(Function),
-      {}
-    );
-  });
+  describe('signal handling', () => {
+    async function shutdown(signal) {
+      const [, handler] = process.on.mock.calls.find(
+        ([name]) => name === signal
+      );
 
-  it('creates a video for each message', async () => {
-    const url = 'https://localhost/playback/123456';
-    const duration = 59;
-    await handle({ content: marshal({ url, duration }) });
-    expect(record).toHaveBeenCalledWith(url, duration);
-  });
+      await handler();
+    }
 
-  it('acknowledges messages on success', async () => {
-    const payload = marshal({ url: '', duration: 12 });
-    const message = { id: 'test-success', content: marshal(payload) };
-    await handle(message);
-    expect(channel.ack).toHaveBeenCalledWith(message);
-  });
+    beforeEach(async () => {
+      await invoke({});
+    });
 
-  it('rejects messages on failure', async () => {
-    record.mockRejectedValue(new Error('Test Error'));
-    const message = { id: 'test-failure' };
-    await handle(message);
-    expect(channel.nack).toHaveBeenCalledWith(message);
-  });
+    [
+      ['SIGINT', 130],
+      ['SIGQUIT', 131],
+      ['SIGTERM', 143],
+    ].forEach(([signal, code]) => {
+      it(`sets up a trap for ${signal}`, async () => {
+        expect(process.on).toHaveBeenCalledWith(signal, expect.any(Function));
+      });
 
-  [
-    ['SIGINT', 130],
-    ['SIGQUIT', 131],
-    ['SIGTERM', 143],
-  ].forEach(([signal, code]) => {
-    it.todo(`sets up a trap for ${signal}`);
+      it(`cleans up and exits normally on ${signal}`, async () => {
+        await shutdown(signal);
 
-    it.todo(`cleans up and exits with 0 on ${signal}`);
+        expect(channel.close).toHaveBeenCalled();
+        expect(connection.close).toHaveBeenCalled();
+        expect(process.exit).not.toHaveBeenCalled();
+      });
 
-    it.todo(`exits with ${code} `);
+      it(`exits with ${code} if cleanup fails on ${signal}`, async () => {
+        channel.close.mockRejectedValue();
+
+        await shutdown(signal);
+
+        expect(process.exit).toHaveBeenCalledWith(code);
+      });
+    });
   });
 });

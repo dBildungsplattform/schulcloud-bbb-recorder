33cee742dcfa5227eb8a08291617ffd4e7eb89fa
Paul Meinhard <paul@bitcrowd.net>
2020-05-04 11:41:57 +0200

    Sketch message handling/processing of recordings

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const childproc = require('child_process');
 const amqp = require('amqplib');
+const path = require('path');
+const util = require('util');
+
+const bbbconf = require('./bbb-recorder/config.json');
+
+const RECORD_CWD = path.resolve(__dirname, 'bbb-recorder');
+const RECORD_DIRECTORY = path.resolve(bbbconf.copyToPath);
+const RECORD_FILENAME = 'export.webm';
 
 const { AMQP_URI, AMQP_QUEUE } = process.env;
+
+const exec = util.promisify(childproc.exec);
 
 function trap(signal, handler, code) {
   process.on(signal, async () => {
@@ -10,6 +21,17 @@ function trap(signal, handler, code) {
       process.exit(code);
     }
   });
+}
+
+async function record(url, duration) {
+  // TODO: assert `duration` is an integer
+  // TODO: assert `url` is a valid HTTP(S) URL
+
+  await exec(`node export.js '${url}' '${RECORD_FILENAME}' '${duration}'`, {
+    cwd: RECORD_CWD,
+  });
+
+  return path.join(RECORD_DIRECTORY, RECORD_FILENAME);
 }
 
 (async function main() {
@@ -33,10 +55,20 @@ function trap(signal, handler, code) {
   // Ensure the queue is created.
   await channel.assertQueue(AMQP_QUEUE, { durable: true });
 
+  // Limit how many messages are processed concurrently
+  await channel.prefetch(1);
+
   // Process each individual message.
-  const handle = (message) => {
+  const handle = async (message) => {
     const payload = JSON.parse(message.content.toString());
-    console.log(payload); // TODO: Acknowledge handled messages
+
+    const filepath = await record(payload.url, payload.duration);
+    console.debug(filepath);
+
+    // TODO: upload
+    // TODO: clean-up?
+
+    // TODO: Acknowledge handled messages
   };
 
   // Start to consume queued messages.
